b9732156c8b2d1876e1b722e787c4f2b3cd73072
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-16 20:02:13 +0100

    remove whitespace

--- resources/views/components/helplink.blade.php
+++ resources/views/components/helplink.blade.php
@@ -1,4 +1,4 @@
 @php $help = AscentCreative\Help\Models\FieldHelp::forKey($key, $title); @endphp
 @if($help->title && $help->text)
-    <a href="{{ route('help.modal', ['key'=>$key]) }}" class="modal-link bi-question-circle-fill text-secondary"> </a>
+    <a href="{{ route('help.modal', ['key'=>$key]) }}" class="modal-link bi-question-circle-fill text-secondary"></a>
 @endif

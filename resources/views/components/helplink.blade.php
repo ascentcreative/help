da79bb29a02881873b0e5e72969e82058010d209
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-03-23 22:47:53 +0000

    Allow link content to be added

--- resources/views/components/helplink.blade.php
+++ resources/views/components/helplink.blade.php
@@ -1,4 +1,4 @@
 @php $help = AscentCreative\Help\Models\FieldHelp::forKey($key, $title); @endphp
 @if($help->title && $help->text)
-    <a href="{{ route('help.modal', ['key'=>$key]) }}" class="modal-link bi-question-circle-fill text-secondary"></a>
+    <a href="{{ route('help.modal', ['key'=>$key]) }}" class="modal-link @if($slot->isEmpty()) text-secondary @endif">@if($slot->isNotEmpty()){{ $slot }}@else <i class="bi-question-circle-fill"></i> @endif</a>
 @endif
